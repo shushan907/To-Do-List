6907be500deff0723756ad1cfcbc28b9409c8d2e
Shushan907 <shushan907@gmail.com>
2020-06-15 01:06:21 +0400

    create function for changing background image

--- js/script.js
+++ js/script.js
@@ -6,6 +6,8 @@ const setQS = (selector, value) => {
     document.querySelector(selector).innerText = value; 
 };
 
+const dayInfo = document.querySelector('.dayInfo');
+const URL = ["url('../image/morning.jpg')","url('../image/afternoon.jpg')", "url('../image/evening.jpg')", "url('../image/night.jpg')"];
 (function () {
     const day = new Date();
     function addZero(i) {
@@ -15,5 +17,18 @@ const setQS = (selector, value) => {
         return i;
     }
     setQS('.day', `${months[day.getMonth()]} ${day.getDate()}, ${days[day.getDay()]}`);
-    setQS('.time', `${addZero(day.getHours())} : ${addZero(day.getMinutes())} : ${addZero(day.getSeconds())}`)
+    setQS('.time', `${addZero(day.getHours())} : ${addZero(day.getMinutes())} : ${addZero(day.getSeconds())}`);
+    changeBackground(day.getHours());
 })();
+
+function changeBackground(hour) {
+    if(hour > 5 && hour < 13) {
+        dayInfo.style.backgroundImage = URL[0];
+    } else if (hour > 12 && hour < 19) {
+        dayInfo.style.backgroundImage = URL[1];
+    } else if (hour > 18 && hour < 23) {
+        dayInfo.style.backgroundImage = URL[2];
+    } else {
+        dayInfo.style.backgroundImage = URL[3];
+    }
+};
