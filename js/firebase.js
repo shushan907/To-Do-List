b9719e33b1892240801af2b235af848eaa1ecc72
Shushan907 <shushan907@gmail.com>
2020-06-17 18:46:04 +0400

    create firebase.js

---
+++ js/firebase.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDZACYmVZPKuaq8faqEsOUbksggSMbhmZE",
+    authDomain: "todo-list-eddab.firebaseapp.com",
+    databaseURL: "https://todo-list-eddab.firebaseio.com",
+    projectId: "todo-list-eddab",
+    storageBucket: "todo-list-eddab.appspot.com",
+    messagingSenderId: "472624710617",
+    appId: "1:472624710617:web:d40b8e459389e751d68fc8",
+    measurementId: "G-LFTR5TB6Y0"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
